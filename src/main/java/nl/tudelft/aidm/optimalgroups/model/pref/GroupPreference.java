ef54804d2901046f65ac6dfbd8676938f3d3482a
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2019-05-15 11:36:24 +0200

    implement db-stored group and project preferences

--- src/main/java/nl/tudelft/aidm/optimalgroups/model/pref/GroupPreference.java
+++ src/main/java/nl/tudelft/aidm/optimalgroups/model/pref/GroupPreference.java
@@ -1,22 +1,61 @@
 package nl.tudelft.aidm.optimalgroups.model.pref;
 
-import javax.sql.DataSource;
+import org.sql2o.Query;
+import org.sql2o.ResultSetHandler;
+import org.sql2o.Sql2o;
 
-public class GroupPreference
+import javax.sql.DataSource;
+import java.util.List;
+
+public interface GroupPreference
 {
 	// TODO: determine representation (let algo guide this choice)
+	int[] asArray();
 
-	public static class fromDb extends GroupPreference
+	class fromDb implements GroupPreference
 	{
 		private DataSource dataSource;
 		private String userId;
 		private String courseEditionId;
+
+		private int[] preference = null;
 
 		public fromDb(DataSource dataSource, String userId, String courseEditionId)
 		{
 			this.dataSource = dataSource;
 			this.userId = userId;
 			this.courseEditionId = courseEditionId;
+		}
+
+		@Override
+		public int[] asArray()
+		{
+			if (preference == null)
+			{
+				preference = fetchFromDb().stream()
+					.mapToInt(Integer::intValue)
+					.toArray();
+			}
+
+			return preference;
+		}
+
+		private List<Integer> fetchFromDb()
+		{
+			final var sql = "SELECT student_id " +
+				"FROM student_preferences " +
+				"WHERE user_id = :userId AND course_edition_id = :courseEditionId";
+
+			var sql2o = new Sql2o(dataSource);
+			try (var conn = sql2o.open())
+			{
+				Query query = conn.createQuery(sql)
+					.addParameter("userId", userId)
+					.addParameter("courseEditionId", courseEditionId);
+
+				List<Integer> preferredStudents = query.executeAndFetch((ResultSetHandler<Integer>) resultSet -> resultSet.getInt("student_id"));
+				return preferredStudents;
+			}
 		}
 	}
 }
--- src/main/java/nl/tudelft/aidm/optimalgroups/model/pref/ProjectPreference.java
+++ src/main/java/nl/tudelft/aidm/optimalgroups/model/pref/ProjectPreference.java
@@ -1,22 +1,62 @@
 package nl.tudelft.aidm.optimalgroups.model.pref;
 
-import javax.sql.DataSource;
+import org.sql2o.Query;
+import org.sql2o.ResultSetHandler;
+import org.sql2o.Sql2o;
 
-public class ProjectPreference
+import javax.sql.DataSource;
+import java.util.List;
+
+public interface ProjectPreference
 {
 	// TODO: determine representation (let algo guide this choice)
+	int[] asArray();
 
-	public static class fromDb extends ProjectPreference
+	class fromDb implements ProjectPreference
 	{
 		private final DataSource dataSource;
 		private final String userId;
 		private final String courseEditionId;
+
+		private int[] preferences = null;
 
 		public fromDb(DataSource dataSource, String userId, String courseEditionId)
 		{
 			this.dataSource = dataSource;
 			this.userId = userId;
 			this.courseEditionId = courseEditionId;
+		}
+
+		@Override
+		public int[] asArray()
+		{
+			if (preferences == null)
+			{
+				preferences = fetchFromDb().stream()
+					.mapToInt(Integer::intValue)
+					.toArray();
+			}
+
+			return preferences;
+		}
+
+		private List<Integer> fetchFromDb()
+		{
+			Sql2o sql2o = new Sql2o(dataSource);
+			try (var conn = sql2o.open())
+			{
+				final var sql = "SELECT project_id " +
+					"FROM project_preferences " +
+					"WHERE user_id = :userId AND course_edition_id = :courseEditionId " +
+					"ORDER BY priority";
+
+				Query query = conn.createQuery(sql)
+					.addParameter("userId", userId)
+					.addParameter("courseEditionId", courseEditionId);
+
+				List<Integer> prefs = query.executeAndFetch((ResultSetHandler<Integer>) resultSet -> resultSet.getInt("project_id"));
+				return prefs;
+			}
 		}
 	}
 }
