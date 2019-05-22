222c8ae76492349cddf3316a9ec00f9873a789a4
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2019-05-22 12:53:53 +0200

    Projects from db with proj. slots implementation

--- src/main/java/nl/tudelft/aidm/optimalgroups/model/entity/Project.java
+++ src/main/java/nl/tudelft/aidm/optimalgroups/model/entity/Project.java
@@ -1,13 +1,74 @@
 package nl.tudelft.aidm.optimalgroups.model.entity;
 
-public class Project
-{
-	public String name;
-	public final int numSlots;
+import java.util.ArrayList;
+import java.util.List;
 
-	public Project(String name, int numSlots)
+public interface Project
+{
+	String name();
+	List<ProjectSlot> slots();
+
+	interface ProjectSlot
 	{
-		this.name = name;
-		this.numSlots = numSlots;
+		String id();
+		Project belongsToProject();
+
+		class Simple implements ProjectSlot {
+
+			private String id;
+			private Project projectBelongsTo;
+
+			public Simple(String id, Project projectBelongsTo)
+			{
+				this.id = id;
+				this.projectBelongsTo = projectBelongsTo;
+			}
+
+			public String id()
+			{
+				return id;
+			}
+
+			public Project belongsToProject()
+			{
+				return projectBelongsTo;
+			}
+		}
+	}
+
+	/**
+	 * Project with a default / hardcoded amount of slots
+	 */
+	class withDefaultSlots implements Project
+	{
+		private final static int numSlots = 5;
+		private int id;
+		private List<ProjectSlot> slots;
+
+		public withDefaultSlots(int id)
+		{
+			this.id = id;
+
+			slots = new ArrayList<>(numSlots);
+			for (int i = 0; i < numSlots; i++)
+			{
+				var slotId = String.format("proj-%d_slot-%d", id, i);
+				var slot = new ProjectSlot.Simple(slotId, this);
+
+				slots.add(slot);
+			}
+		}
+
+		@Override
+		public String name()
+		{
+			return "proj_" + String.valueOf(id);
+		}
+
+		@Override
+		public List<ProjectSlot> slots()
+		{
+			return slots;
+		}
 	}
 }
--- src/main/java/nl/tudelft/aidm/optimalgroups/model/entity/Projects.java
+++ src/main/java/nl/tudelft/aidm/optimalgroups/model/entity/Projects.java
@@ -1,33 +1,89 @@
 package nl.tudelft.aidm.optimalgroups.model.entity;
 
+import org.sql2o.Query;
+import org.sql2o.ResultSetHandler;
+import org.sql2o.Sql2o;
+
+import javax.sql.DataSource;
 import java.util.List;
 
-public class Projects
+public interface Projects
 {
-	private List<Project> projectList;
+	int count();
+	int countAllSlots();
+//	Project withIndex();
 
-	public int count()
+	class InDb implements Projects
 	{
-		return projectList.size();
-	}
+		private DataSource dataSource;
+		private int courseEditionId;
 
-	public Project withIndex(int idx)
-	{
-		return projectList.get(idx);
-	}
+		private List<Project> projectList = null;
 
-	private int numTotalSlots;
-	public int numTotalSlots()
-	{
-		// lazy eval
-		if (numTotalSlots < 0)
+		InDb(DataSource dataSource, int courseEditionId)
 		{
-			numTotalSlots = projectList.stream()
-				.map(project -> project.numSlots)
-				.mapToInt(Integer::intValue)
-				.sum();
+			this.dataSource = dataSource;
+			this.courseEditionId = courseEditionId;
 		}
 
-		return numTotalSlots;
+		public int count()
+		{
+			return projectList().size();
+		}
+
+		protected List<Project> projectList()
+		{
+			if (projectList == null)
+			{
+				projectList = fetchFromDb();
+			}
+
+			return projectList;
+		}
+
+//		public Project withIndex(int idx)
+//		{
+//			return projectList.get(idx);
+//		}
+
+		private int numTotalSlots = -1;
+
+		@Override
+		public int countAllSlots()
+		{
+			// lazy eval
+			if (numTotalSlots < 0)
+			{
+				numTotalSlots = projectList.stream()
+					.map(project -> project.numSlots)
+					.mapToInt(Integer::intValue)
+					.sum();
+			}
+
+			return numTotalSlots;
+		}
+
+		private List<Project> fetchFromDb()
+		{
+			var sql = "SELECT id FROM test.projects where course_edition_id = :courseEditionId";
+			try (var connection = new Sql2o(dataSource).open())
+			{
+				Query query = connection.createQuery(sql);
+				query.addParameter("courseEditionId", courseEditionId);
+
+				List<Project> projectsAsList = query.executeAndFetch(
+					(ResultSetHandler<Project>) rs ->
+						new Project.withDefaultSlots(rs.getInt("id"))
+				);
+
+				return projectsAsList;
+			}
+		}
+
+	}
+
+	public static Projects fromDb(DataSource dataSource, int courseEditionInt)
+	{
+		return new InDb(dataSource, courseEditionInt);
 	}
 }
