7a1107fb0bd87945baad219c930c4a66db46d446
maxdekrieger <maxdekrieger@hotmail.com>
2019-05-29 02:29:46 +0200

    finished algo, needs testing

--- src/main/java/nl/tudelft/aidm/optimalgroups/algorithm/group/BepSysWithRandom.java
+++ src/main/java/nl/tudelft/aidm/optimalgroups/algorithm/group/BepSysWithRandom.java
@@ -6,16 +6,20 @@ public class BepSysWithRandom
     Map<String, Agent> availableStudents;
     Map<String, Agent> unavailableStudents;
     List<Group> groups;
+    int maxGroupSize;
+    int minGroupSize;
 
     // Pass the list of students to make groups from
-    public BepSysWithRandom(List<Agent> availableStudents) {
+    public BepSysWithRandom(List<Agent> availableStudents, int maxGroupSize, int minGroupSize) {
         this.availableStudents = new HashMap<String, Agent>();
         this.unavailableStudents = new HashMap<String, Agent>();
+        this.groups = new ArrayList<Group>;
+        this.maxGroupSize = maxGroupSize;
+        this.minGroupSize = minGroupSize;
 
         for (Agent a : availableStudents) {
             this.availableStudents.put(a.name, a);
         }
-        this.groups = new ArrayList<Group>;
     }
 
     public void constructGroups() {
@@ -152,7 +156,7 @@ public class BepSysWithRandom
     private void pickBestGroups(List<PossibleGroup> possibleGroups) {
         Collections.sort(possibleGroups, new Comparator<PossibleGroup>() {
             int compare(PossibleGroup left, PossibleGroup right)  {
-                return left.score - right.score;
+                return right.score - left.score;
             }
         });
         
@@ -184,12 +188,180 @@ public class BepSysWithRandom
     }
 
     private void mergeGroups() {
-        // TO-DO
+        List<Group> merged = new ArrayList<Group>();
+        List<Group> unmerged = new ArrayList<Group>();
+        for (Group g : this.groups) {
+            if (g.members.asCollection().size() == this.maxGroupSize) {
+                merged.add(g);
+            } else {
+                unmerged.add(g);
+            }
+        }
+
+        int count = merged.size();
+        int numberOfStudents = this.students.size();
+        int groupsMax = numberOfStudents / this.maxGroupSize;
+        int remainder = numberOfStudents % this.maxGroupSize;
+        int numberOfGroups = groupsMax + remainder / this.minGroupSize;
+        remainder = remainder % this.minGroupSize;
+
+        while (remainder > 0) {
+            groupsMax -= 1;
+            remainder += this.maxGroupSize;
+            numberOfGroups += remainder / this.minGroupSize;
+            remainder = remainder % this.minGroupSize;
+        }
+
+        Collections.sort(unmerged, new Comparator<PossibleGroup>() {
+            int compare(Group left, Group right)  {
+                return left.members.asCollection().size() - right.members.asCollection().size();
+            }
+        });
+
+        while (unmerged.size() > 0) {
+            Group unmergedGroup = unmerged.get(0);
+            int unmergedGroupSize = unmergedGroup.members.asCollection().size();
+            unmerged.remove(0);
+            Map<Integer, Integer> scores = new HashMap<Integer, Integer>();
+            for (Group otherUnmergedGroup : unmerged) {
+                int together = unmergedGroupSize + otherUnmergedGroup.members.asCollection().size();
+
+                // Only keep scores if the size is equal to the maximum group size
+                if (together == this.maxGroupSize && count < groupsMax) {
+                    scores.put(computePrefScore(unmergedGroup, otherUnmergedGroup), otherUnmergedGroup);
+                }
+            }
+            if (scores.size() == 0) {
+                for (Group otherUnmergedGroup : unmerged) {
+                    int together = unmergedGroupSize + otherUnmergedGroup.members.asCollection().size();
+
+                    // Keep scores if it does not exceed the maximum group size
+                    if (together <= this.maxGroupSize) {
+                        scores.put(computePrefScore(unmergedGroup, otherUnmergedGroup), otherUnmergedGroup);
+                    }
+                }
+            }
+            if (scores.size() == 0) {
+                if (unmergedGroupSize < this.minGroupSize) {
+                    // Divide all people of this group
+                    for (Agent a : unmergedGroup.members.asCollection()) {
+                        Group newGroup = new Group(0, Agents.from(Arrays.asList(a)), null); //TO-DO: add sensible id here
+                        unmerged.add(newGroup);
+                    }
+                } else {
+                    merged.add(unmergedGroup);
+                    continue;
+                }
+            }
+
+            // Get partner group
+            int maxScore = Collections.max(scores.keySet());
+            Group partnerGroup = scores.get(maxScore);
+            unmerged.remove(partnerGroup);
+
+            // Construct new merged group
+            ArrayList newMembers = new ArrayList<Agent>();
+            newMembers.addAll(unmergedGroup.members.asCollection());
+            newMembers.addAll(partnerGroup.members.asCollection());
+            Group newGroup = new Group(0, Agents.from(newMembers), null); //TO-DO: add sensible id here
+            
+            int newGroupSize = newGroup.members.asCollection().size();
+            if (newGroupSize < this.maxGroupSize) {
+                unmerged.add(newGroup);
+            } else if (newGroupSize == this.maxGroupSize) {
+                count += 1;
+                merged.add(newGroup);
+            } else {
+                throw new Exception("Group size is somehow larger than maximum group size");
+            }
+        }
+        this.groups = merged;
+    }
+
+    private int computePrefScore(Group g1, Group g2) {
+        /*
+        score = 0
+        g1users = g1[:members].each { |m| m } + [g1[:leader]]
+        g2users = g2[:members].each { |m| m } + [g2[:leader]]
+
+        g1prefs = comp_average_pref g1users
+        g2prefs = comp_average_pref g2users
+
+        g1sorted = g1prefs.sort_by { |_k, v| v }.map { |v| v[0] }
+        g2sorted = g2prefs.sort_by { |_k, v| v }.map { |v| v[0] }
+
+        cnt = 0
+        g1sorted.each do |p|
+        cnt2 = g2sorted.index(p)
+        score += (cnt - cnt2) * (cnt - cnt2) unless cnt2.nil?
+        cnt += 1
+        end
+
+        score
+        */
+        int score = 0;
+        Map<Integer, Integer> g1Prefs = computeAveragePrefs(g1)
+            .entrySet()
+            .stream()
+            .sorted(comparingByValue())
+            .collect(
+                toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                    LinkedHashMap::new));
+        
+        Map<Integer, Integer> g2Prefs = computeAveragePrefs(g2)
+                    .entrySet()
+                    .stream()
+                    .sorted(comparingByValue())
+                    .collect(
+                        toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                            LinkedHashMap::new));
+
+        ArrayList<Integer> g1Sorted = g1Prefs.keySet();
+        ArrayList<Integer> g2Sorted = g2Prefs.keySet();
+
+        int count = 0;
+        for (Integer i : g1Sorted) {
+            int count2 = g2sorted.indexOf(i);
+            if (count2 != -1) {
+                score += (count - count2) * (count - count2)
+            }
+            count += 1;
+        }
+
+        return score;
+    }
+
+    private Map<Integer, Integer> computeAveragePrefs(Group g) {
+        Map<Integer, Integer> prefs = new LinkedHashMap<Integer, Integer>();
+        int prefCounter = 0;
+        for (Agent a : g.members().asCollection()) {
+            int[] preferences = a.projectPreference.fromDb.asArray();
+            if (preferences.length > 0) {
+                prefCounter += 1;
+            }
+
+            for (int priority = 0; priority < preferences.length; priority++) {
+                int project = preferences[priority];
+                Integer currentPreferences = prefs.get(project);
+                if (currentPreferences == null) {
+                    currentPreferences = 0;
+                }
+                prefs.put(project, currentPreferences + priority);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : prefs) {
+            int avgPref = 4;
+            if (prefCounter > 0) {
+                avgPref = entry.getValue() / prefCounter;
+            }
+            entry.setValue(avgPref);
+        }
+        return prefs;
     }
 
     private List<Agent> getAvailableFriends(Agent a) {
         List<Agent> friends = new ArrayList<Agent>();
-        for (int i : pair.getValue().groupPreference.asArray()) {
+        for (int i : a.groupPreference.asArray()) {
             if (this.availableStudents.containsKey(String.valueOf(i))) {
                 friends.add(this.availableStudents.get(i));
             }
