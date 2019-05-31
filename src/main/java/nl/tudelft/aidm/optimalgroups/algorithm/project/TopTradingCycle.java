9d39aead93910e308d9001c42287bb8aeb11bf92
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2019-05-31 19:58:21 +0200

    fix compilation error with TTC and SerialDictatorship

--- src/main/java/nl/tudelft/aidm/optimalgroups/algorithm/project/TopTradingCycle.java
+++ src/main/java/nl/tudelft/aidm/optimalgroups/algorithm/project/TopTradingCycle.java
@@ -1,11 +1,12 @@
 package nl.tudelft.aidm.optimalgroups.algorithm.project;
 
-import nl.tudelft.aidm.optimalgroups.model.entity.Agents;
+import nl.tudelft.aidm.optimalgroups.model.entity.Group;
+import nl.tudelft.aidm.optimalgroups.model.entity.Project;
 
 public class TopTradingCycle implements ProjectMatchingAlgorithm
 {
 	@Override
-	public Matching doMatching(Agents agents)
+	public Matching<Group, Project> result()
 	{
 		return null;
 	}
