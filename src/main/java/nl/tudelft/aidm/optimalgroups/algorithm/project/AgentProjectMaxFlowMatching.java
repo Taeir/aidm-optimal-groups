0382f41e0117a1bd2c3172f1a91123d540cecef9
Philipe Louchtch - de Raadt <philipelouchtch@outlook.com>
2020-07-15 10:48:09 +0200

    simplify and speedup maxflow (ortools version)

--- src/main/java/nl/tudelft/aidm/optimalgroups/algorithm/project/AgentProjectMaxFlowMatching.java
+++ src/main/java/nl/tudelft/aidm/optimalgroups/algorithm/project/AgentProjectMaxFlowMatching.java
@@ -7,7 +7,6 @@ import nl.tudelft.aidm.optimalgroups.model.agent.Agent;
 import nl.tudelft.aidm.optimalgroups.model.agent.Agents;
 import nl.tudelft.aidm.optimalgroups.model.dataset.DatasetContext;
 import nl.tudelft.aidm.optimalgroups.model.matching.AgentToProjectMatching;
-import nl.tudelft.aidm.optimalgroups.model.pref.ProjectPreference;
 import nl.tudelft.aidm.optimalgroups.model.project.Project;
 import nl.tudelft.aidm.optimalgroups.model.project.Projects;
 import nl.tudelft.aidm.optimalgroups.model.matching.AgentToProjectMatch;
@@ -15,6 +14,7 @@ import nl.tudelft.aidm.optimalgroups.model.matching.Match;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 @SuppressWarnings({"Duplicates"})
 public class AgentProjectMaxFlowMatching implements AgentToProjectMatching
@@ -24,11 +24,6 @@ public class AgentProjectMaxFlowMatching implements AgentToProjectMatching
 	}
 
 	private static Map<Collection<Project>, AgentProjectMaxFlowMatching> existingResultsCache = new ConcurrentHashMap<>();
-
-	// source and sink vertices
-	private static Vertex<Object> source = new Vertex<>(null);
-	private static Vertex<Object> sink = new Vertex<>(null);
-
 
 	private final DatasetContext datasetContext;
 	public final Agents students;
@@ -116,8 +111,6 @@ public class AgentProjectMaxFlowMatching implements AgentToProjectMatching
 	@Override
 	public DatasetContext datasetContext()
 	{
-		// FIXME later
-//		throw new ImplementMe();
 		return this.datasetContext;
 	}
 
@@ -159,8 +152,6 @@ public class AgentProjectMaxFlowMatching implements AgentToProjectMatching
 		return allStudentsAreMatched;
 	}
 
-
-	// quick and dirty: want access to groupedByProject but also keep the asList() method functioning AND without unnecessary recomputing values each time
 	private void init()
 	{
 		// Very simple check: init only once, subsequent calls return directly
@@ -168,168 +159,103 @@ public class AgentProjectMaxFlowMatching implements AgentToProjectMatching
 			return;
 		}
 
-		var groupedByProject = new IdentityHashMap<Project, List<Agent>>(projects.count());
-
-		StudentVertices studentVertices = new StudentVertices(students);
-		ProjectVertices projectVertices = new ProjectVertices(projects);
-		var edges = new StudentToProjectEdges(studentVertices, projectVertices);
-
 		MinCostFlow minCostFlow = new MinCostFlow();
 
-		int source = AgentProjectMaxFlowMatching.source.id;
-		int sink = AgentProjectMaxFlowMatching.sink.id;
+		int vertId = 0;
+		int source = vertId++;
+		int sink = vertId++;
 
 		// Source and Sink do not need to supply/consume more than we have students
-		minCostFlow.setNodeSupply(source, studentVertices.count());
-		minCostFlow.setNodeSupply(sink, -studentVertices.count());
+		minCostFlow.setNodeSupply(source, students.count());
+		minCostFlow.setNodeSupply(sink, -students.count());
 
-		studentVertices.forEach(studentVertex -> {
-			minCostFlow.addArcWithCapacityAndUnitCost(source, studentVertex.id, 1, 1);
+		var studentVerts = new StudentVertices(students, vertId);
+		vertId += students.count();
+
+		var projectVerts = new ProjectVertices(projects, vertId);
+
+		var studentProjectArcs = new ArrayList<FlowArc>(students.count() * projects.count());
+
+		students.forEach(student -> {
+			Integer studentVert = studentVerts.toVertex.get(student);
+			minCostFlow.addArcWithCapacityAndUnitCost(source, studentVert, 1, 1);
+
+			var prefs = student.projectPreference();
+			student.projectPreference().forEach((Project project, int rank) -> {
+
+				var projectVert = projectVerts.vertex.get(project);
+
+				if (projectVert != null)
+				{
+					var cost = preferencesToCostFunction.costOfGettingAssigned(prefs, project);
+
+					var arc = minCostFlow.addArcWithCapacityAndUnitCost(studentVert, projectVert, 1, cost);
+					studentProjectArcs.add(new FlowArc(student, project, arc));
+				}
+			});
 		});
 
-		projectVertices.forEach(projectVertex -> {
-			Project project = projectVertex.content().theProject;
-			int capacity = project.slots().size() * datasetContext.groupSizeConstraint().maxSize();
-			minCostFlow.addArcWithCapacityAndUnitCost(projectVertex.id, sink, capacity, 1);
+		int maxGroupSize = datasetContext.groupSizeConstraint().maxSize();
+		projects.forEach(project -> {
+			int numSlots = project.slots().size();
+			int capacity = numSlots * maxGroupSize;
+			var projectVert = projectVerts.vertex.get(project);
+			minCostFlow.addArcWithCapacityAndUnitCost(projectVert, sink, capacity, 1);
 		});
 
+		var status = minCostFlow.solveMaxFlowWithMinCost();
 
-		List<Integer> arcs = new ArrayList<>();
-
-		edges.forEach(edge -> {
-			// Convert the edge to an OrTools MaxFlow arc with capacity 1 and cost set to the weight of the edge (the rank of the preference this edge represents)
-			int arc = minCostFlow.addArcWithCapacityAndUnitCost(edge.from.id, edge.to.id, 1, edge.weight);
-
-			// We need to record the id's of the arcs created by OrTools MaxFlow implementation
-			// as we need to query them after the problem instance was solved.
-			arcs.add(arc);
-		});
-
-		// TODO: check if status (return value) is always "OPTIMAL"?
-		minCostFlow.solveMaxFlowWithMinCost();
-
-		for (var arc : arcs)
+		var groupedByProject = new IdentityHashMap<Project, List<Agent>>(projects.count());
+		for (var arc : studentProjectArcs)
 		{
 			// Not all arcs will have a flow assigned, we only care about those that do (non-zero flow: vertices are matched)
-			if (minCostFlow.getFlow(arc) == 0) continue;
+			if (minCostFlow.getFlow(arc.id) == 0) continue;
 
-			int from = minCostFlow.getTail(arc);
-			int to = minCostFlow.getHead(arc);
-
-			// Not very efficient: getting the agent/project instance for the corresponding ID
-			Agent student = studentVertices.asReadonlyList().stream().filter(v -> v.id == from).findAny().get().content().theStudent;
-			Project project = projectVertices.asReadonlyList().stream().filter(v -> v.id == to).findAny().get().content().theProject;
-
-			// Put in the grouping
-			groupedByProject.computeIfAbsent(project, __ -> new ArrayList<>()).add(student);
+			groupedByProject.computeIfAbsent(arc.project, __ -> new ArrayList<>())
+				.add(arc.student);
 		}
 
 		this.groupedByProject = groupedByProject;
 	}
 
-	///////////
-	/* EDGES */
-	///////////
-	private class StudentToProjectEdges extends DirectedWeightedEdges<StudentProjectMatchingVertexContent> // no generic, we'll cast
+	private static class StudentVertices
 	{
-		public StudentToProjectEdges(StudentVertices studentVertices, ProjectVertices projectVertices)
+		Map<Agent, Integer> toVertex;
+		Map<Integer, Agent> toAgent;
+
+		public StudentVertices(Agents agents, int id)
 		{
-			// for each student
-			studentVertices.forEach(studentVertex -> {
+			toAgent = new IdentityHashMap<>(agents.count());
+			toVertex = new IdentityHashMap<>(agents.count());
 
-				// for each student's preference, create the edges to projects according to preferences
-				Agent student = studentVertex.content().theStudent;
-
-				if (student.projectPreference().isCompletelyIndifferent()) {
-					// Indifferent -> no projects in pref profile at all
-					projectVertices.forEach(projectVertex -> {
-						// This student is indifferent, therefore prioritize everyone else by assigning lowest rank
-						// the "combined preferences" algorithm does the same. Another approach: exclude these studens from
-						// the maxflow matchings and only add them at the very end as "wildcard" students
-						// TODO: investigate effects
-						int rank = projectVertices.count() - 1;
-
-						var edge = DirectedWeightedEdge.between(studentVertex, projectVertex, rank);
-						add(edge);
-					});
-				}
-				else {
-					// Note: if student is missing projects from the profile, no edge will be created
-					// therefore projects that are missing from the pref profile are counted as "do not want"
-					student.projectPreference().forEach((Project project, int rank) -> {
-						projectVertices.findForProject(project.id())
-							.ifPresent(projectVertex -> {
-								var costOfProjectToStudent = preferencesToCostFunction.costOfGettingAssigned(student.projectPreference(), project);
-								var edge = DirectedWeightedEdge.between(studentVertex, projectVertex, costOfProjectToStudent);
-								add(edge);
-							});
-					});
-
-				}
-			});
+			for (var agent : agents.asCollection()) {
+				toAgent.put(id, agent);
+				toVertex.put(agent, id);
+				id++;
+			}
 		}
 	}
 
-	//////////////
-	/* VERTICES */
-	//////////////
-	private static class ProjectVertices extends Vertices<ProjectVC>
+	private static class ProjectVertices
 	{
-		private Map<Integer, Vertex<ProjectVC>> projectIdToProjectVert;
+		Map<Project, Integer> vertex;
+		Map<Integer, Project> project;
 
-		public ProjectVertices(Projects projects)
+		public ProjectVertices(Projects projects, int id)
 		{
-			projectIdToProjectVert = new HashMap<>();
+			this.project = new IdentityHashMap<>(projects.count());
+			this.vertex = new IdentityHashMap<>(projects.count());
 
-			projects.asCollection().forEach(project -> {
-				var vert = new Vertex<>(new ProjectVC(project));
-
-				this.listOfVertices.add(vert);
-				this.projectIdToProjectVert.put(project.id(), vert);
-			});
-		}
-
-		public Optional<Vertex<ProjectVC>> findForProject(int projectId)
-		{
-			Vertex<ProjectVC> value = projectIdToProjectVert.get(projectId);
-			return Optional.ofNullable(value);
+			for (var project : projects.asCollection()) {
+				this.project.put(id, project);
+				vertex.put(project, id);
+				id++;
+			}
 		}
 	}
 
-	private static class StudentVertices extends Vertices<StudentVC>
+	private static record FlowArc(Agent student, Project project, int id)
 	{
-		public StudentVertices(Agents students)
-		{
-			students.forEach(student -> {
-				var vert = new Vertex<>(new StudentVC(student));
-				listOfVertices.add(vert);
-			});
-		}
-	}
-
-	/////////////////////
-	/* VERTEX CONTENTS */
-	/////////////////////
-	private interface StudentProjectMatchingVertexContent {}
-
-	private static class ProjectVC implements StudentProjectMatchingVertexContent
-	{
-		final Project theProject;
-
-		public ProjectVC(Project theProject)
-		{
-			this.theProject = theProject;
-		}
-	}
-
-	private static class StudentVC implements StudentProjectMatchingVertexContent
-	{
-		final Agent theStudent;
-
-		public StudentVC(Agent theStudent)
-		{
-			this.theStudent = theStudent;
-		}
 	}
 }
 
