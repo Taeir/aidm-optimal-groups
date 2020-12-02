34ceebe8698f074e64d3931a5db10f7619914e3a
Philipe Louchtch <philipelouchtch@outlook.com>
2020-12-02 12:42:19 +0100

    fix jdrasil tree-decomp test

--- src/main/java/nl/tudelft/aidm/optimalgroups/algorithm/treedecomp/JdrasilTest.java
+++ src/main/java/nl/tudelft/aidm/optimalgroups/algorithm/treedecomp/JdrasilTest.java
@@ -4,9 +4,13 @@ import jdrasil.algorithms.ExactDecomposer;
 import jdrasil.graph.GraphFactory;
 import jdrasil.graph.TreeDecomposer;
 import jdrasil.graph.TreeDecomposition;
+import jdrasil.utilities.logging.JdrasilLogger;
 import nl.tudelft.aidm.optimalgroups.dataset.bepsys.CourseEdition;
 import nl.tudelft.aidm.optimalgroups.model.dataset.DatasetContext;
 import nl.tudelft.aidm.optimalgroups.model.graph.BipartitieAgentsProjectGraph;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class JdrasilTest
 {
@@ -19,6 +23,9 @@ public class JdrasilTest
 
 		jdrasil.graph.Graph<Integer> jdrasilGraph = GraphFactory.emptyGraph();
 
+
+		Logger.getLogger(JdrasilLogger.getName()).setLevel(Level.INFO);
+
 		for (var vert : datasetAsGraph.vertices()) {
 //			Objects.requireNonNull(vert);
 //			jdrasilGraph.addVertex(vert.id());
@@ -27,14 +34,20 @@ public class JdrasilTest
 		for (var edge : datasetAsGraph.edges().all()) {
 //			Objects.requireNonNull(edge);
 //			jdrasilGraph.addDirectedEdge(edge.v().id(), edge.w().id());
-//			if (edge.rank() <= 5) {
+			if (edge.rank() <= 5) {
+				// Bug in jdrasilGraph: must add the vertexes first anyway
+				jdrasilGraph.addVertex(edge.v().id());
+				jdrasilGraph.addVertex(edge.w().id());
+
 				jdrasilGraph.addEdge(edge.v().id(), edge.w().id());
-//			}
+			}
 		}
 
 		TreeDecomposition<Integer> td;
 		TreeDecomposer<Integer> decomposer = new ExactDecomposer<>(jdrasilGraph);
 		td = decomposer.call();
+
+//		var isValid = td.isValid();
 
 		td.getWidth();
 
