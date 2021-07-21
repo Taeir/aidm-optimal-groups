39448fe68628a428d267a2f7b10d2769c08267fc
Philipe Louchtch <philipelouchtch@outlook.com>
2021-07-21 19:38:49 +0200

    condition grouping constraint (tentative) + experiments

---
+++ src/main/java/nl/tudelft/aidm/optimalgroups/algorithm/holistic/chiarandini/constraints/grouping/ConditionalGroupConstraint.java
@@ -0,0 +1,144 @@
+package nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.constraints.grouping;
+
+import gurobi.*;
+import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.constraints.AssignmentConstraints;
+import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.constraints.Constraint;
+import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.model.GurobiHelperFns;
+import nl.tudelft.aidm.optimalgroups.model.group.Group;
+import nl.tudelft.aidm.optimalgroups.model.group.Groups;
+import plouchtch.assertion.Assert;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+/**
+ * The soft-grouping constraint grants the grouping wish or lets the agents go "solo" if grouping is infeasible
+ */
+public class ConditionalGroupConstraint implements Constraint
+{
+	public final Collection<GrpLinkedDecisionVar> violateGroupingDecVars;
+	private final Groups<?> groups;
+	private final int conditionalUpToIncludingRank;
+	
+	public ConditionalGroupConstraint(Groups<?> groups, int conditionalUpToIncludingRank)
+	{
+		violateGroupingDecVars = new ArrayList<>();
+		
+		this.groups = groups;
+		this.conditionalUpToIncludingRank = conditionalUpToIncludingRank;
+	}
+	
+	@Override
+	public void apply(GRBModel model, AssignmentConstraints assignmentConstraints) throws GRBException
+	{
+		var grpIdx = new AtomicInteger(1);
+		
+		groups.forEach(group -> {
+			var projPrefs = group.projectPreference();
+			var agents = new ArrayList<>(group.members().asCollection());
+			
+			// let this be 'g'
+			var violateGroupingDecVar = GrpLinkedDecisionVar.make(group, grpIdx.getAndIncrement(), model);
+			violateGroupingDecVars.add(violateGroupingDecVar);
+				
+			projPrefs.forEach(((project, rank) ->
+			{
+				// For now, this 'if' the only real difference with soft-group constraint
+				if (rank.unacceptable() || rank.asInt() > conditionalUpToIncludingRank)
+					return; // continue
+				
+				project.slots().forEach(slot ->
+				{
+					// the x's (assignment decision var) of all agents in group for assignment to project slot 'slot'
+					var xToSlotVarsAgents = agents.stream().map(agent -> assignmentConstraints.xVars.of(agent, slot))
+						.flatMap(Optional::stream)
+						.map(AssignmentConstraints.X::asVar)
+						.collect(Collectors.toList());
+					
+					Assert.that(xToSlotVarsAgents.size() == agents.size())
+						.orThrowMessage("Could not find all assignment vars for agents in clique to group together...");
+					
+					try
+					{
+						var leaderAssVar = xToSlotVarsAgents.get(0);
+						for (int i = 1; i < agents.size(); i++)
+						{
+							var otherAssVar = xToSlotVarsAgents.get(i);
+							
+						    var lhs = new GRBLinExpr();
+						    
+						    // Let a desired group be: grp = {s1, s2, s3, s4, s5}
+							// Let 'g' be the decision variable for granting the group wish.
+							//
+							//   Using a binary variable to switch a constraint is not something LP/IP can handle,
+							//   luckily, gurobi has a special constraint for this called the indicator constraint that
+							//   is switched on/off by an indicator variable. Defined in their docs as "z -> constr".
+							
+							// To save on the amount of extra constraints added (because the indicator constr is more expensive),
+							// the grouping is done as follows:
+							//    a "leader" member is randomly chosen whose project-slot-assignment decision variable (x_leader)
+							//    is chosen to be linked to the assignment decision variables of everyone else in the group (x2...xn),
+							//    through transitivity this links everyone. Then, these linking constraints are all conditional on the
+							//    decision variable (g) of granting the group wish
+							
+							// Indicator constraint can only handle vars on lhs, so rewrite x_leader = x_2  <==> x_leader - x_2 = 0
+							lhs.addTerm(1.0, otherAssVar);
+							lhs.addTerm(-1.0, leaderAssVar);
+							
+							var constName = "cnstr_%s_%d_%s".formatted(violateGroupingDecVar.name, i, slot.id());
+							
+							// note that g == 0 is indicator for doing the linking, this way we can express not-linking as a big penalty in the objective
+							model.addGenConstrIndicator(violateGroupingDecVar.asVar(), 0,
+								lhs, '=', 0,
+								constName);
+						}
+					}
+					catch (GRBException ex)
+					{
+						throw new RuntimeException(ex);
+					}
+				});
+			}));
+		});
+		
+		var objective = new GRBLinExpr((GRBLinExpr) model.getObjective());
+		
+		var ogSize = objective.size();
+		Assert.that(objective.size() > 0).orThrowMessage("Objective function must be set before adding the Soft Grouping constraint");
+		
+		violateGroupingDecVars.forEach(violateGroupingDecVar -> {
+			// Hefty penalty if the group is unlinked (var == 1)
+			objective.addTerm(100000, violateGroupingDecVar.asVar());
+		});
+		
+		model.setObjective(objective, GRB.MINIMIZE);
+		model.update();
+		
+		var newSize = ((GRBLinExpr) model.getObjective()).size();
+		Assert.that(newSize == ogSize + violateGroupingDecVars.size()).orThrowMessage("Objective did not get updated");
+	}
+	
+	@Override
+	public String simpleName()
+	{
+		return "condgrps";
+	}
+	
+	public record GrpLinkedDecisionVar(Group group, GRBVar softGrpVar, String name)
+	{
+		static GrpLinkedDecisionVar make(Group group, Integer grpIdx, GRBModel model)
+		{
+			String name = "condlink_g" + grpIdx;
+			var softGrpVar = GurobiHelperFns.makeBinaryVariable(model, name);
+			return new GrpLinkedDecisionVar(group, softGrpVar, name);
+		}
+		
+		public GRBVar asVar()
+		{
+			return this.softGrpVar;
+		}
+	}
+}
---
+++ src/main/java/nl/tudelft/aidm/optimalgroups/experiment/grpcnstr/Random_testground.java
@@ -0,0 +1,194 @@
+package nl.tudelft.aidm.optimalgroups.experiment.grpcnstr;
+
+import gurobi.GRB;
+import gurobi.GRBEnv;
+import gurobi.GRBException;
+import gurobi.GRBModel;
+import nl.tudelft.aidm.optimalgroups.algorithm.group.bepsys.partial.GroupsFromCliques;
+import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.constraints.AssignmentConstraints;
+import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.constraints.FixMatchingConstraint;
+import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.constraints.UndominatedByProfileConstraint;
+import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.constraints.grouping.ConditionalGroupConstraint;
+import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.constraints.grouping.GroupConstraint;
+import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.model.ChiarandiniAgentToProjectMatching;
+import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.model.Profile;
+import nl.tudelft.aidm.optimalgroups.algorithm.holistic.chiarandini.objectives.OWAObjective;
+import nl.tudelft.aidm.optimalgroups.dataset.bepsys.CourseEdition;
+import nl.tudelft.aidm.optimalgroups.experiment.dataset.ResearchProject2021Q4Dataset;
+import nl.tudelft.aidm.optimalgroups.experiment.researchproj.TwoRoundExperimentReport;
+import nl.tudelft.aidm.optimalgroups.model.agent.Agents;
+import nl.tudelft.aidm.optimalgroups.model.dataset.DatasetContext;
+import nl.tudelft.aidm.optimalgroups.model.dataset.sequentual.SequentualDatasetContext;
+import nl.tudelft.aidm.optimalgroups.model.group.Groups;
+import nl.tudelft.aidm.optimalgroups.model.matchfix.MatchFixes;
+import nl.tudelft.aidm.optimalgroups.model.matching.AgentToProjectMatching;
+import plouchtch.assertion.Assert;
+import plouchtch.functional.actions.Rethrow;
+import plouchtch.util.Try;
+
+import java.io.File;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+public class Random_testground
+{
+	public static void main(String[] args)
+	{
+		var ce = 10;
+		
+		var dataset = CourseEdition.fromLocalBepSysDbSnapshot(ce);
+		
+		var seqDataset = SequentualDatasetContext.from(dataset);
+		var allAgents = seqDataset.allAgents();
+//
+//			var algo = new Chiarandini_Utilitarian_MinSum_IdentityScheme();
+		
+		var maxsizeCliques = new GroupsFromCliques(allAgents).ofSize(seqDataset.groupSizeConstraint().maxSize());
+		
+		// Indifferent agents don't care, don't include them in the profile as they consider any project to be equal.
+		var groupingAgents = maxsizeCliques.asAgents();
+		var indifferentAgents = allAgents.asCollection().stream().filter(agent -> agent.projectPreference().isCompletelyIndifferent()).collect(collectingAndThen(toList(), Agents::from));
+		var individualAgents = allAgents.without(groupingAgents).without(indifferentAgents);
+		
+		var values = maxsizeCliques.asCollection().stream().map(group -> {
+			return group.members().asCollection().stream()
+				       .map(agent -> seqDataset.mapToOriginal(agent))
+				       .map(agent -> agent.id.toString())
+				       .collect(Collectors.joining(", ", "[", "]"));
+		}).collect(Collectors.joining("\n"));
+		
+		
+		try {
+			var env = new GRBEnv();
+			env.start();
+			var model = new GRBModel(env);
+			
+			/*         */
+			/* ROUND 1 */
+			/*         */
+			AssignmentConstraints assignmentConstraints = AssignmentConstraints.createInModel(model, seqDataset);
+			
+			var objFn = new OWAObjective(seqDataset, assignmentConstraints);
+			objFn.apply(model);
+			
+			// process match-fixes
+//			var matchFixesSeq = dataset.matchesToFix().forSequentual(seqDataset);
+//			applyManualMatchFixes(matchFixesSeq, model, assignmentConstraints);
+			
+			model.optimize();
+			
+			// results round 1
+			var matching = new ChiarandiniAgentToProjectMatching(assignmentConstraints.xVars, seqDataset);
+			var profileIndividual = profileOfIndividualAgentsInMatching(seqDataset, individualAgents, matching.sequential());
+			
+			/*         */
+			/* ROUND 2 */
+			/*         */
+			
+//			var grpConstr = new GroupConstraint(maxsizeCliques);
+//			var grpConstr = new SoftGroupConstraint(maxsizeCliques);
+			var grpConstr = new ConditionalGroupConstraint(maxsizeCliques, 10);
+			grpConstr.apply(model, assignmentConstraints);
+			
+			var domConstr = new UndominatedByProfileConstraint(profileIndividual, individualAgents, seqDataset.allProjects());
+			domConstr.apply(model, assignmentConstraints);
+			
+			model.update();
+			model.optimize();
+			
+			// results round 2
+			var matching2 = new ChiarandiniAgentToProjectMatching(assignmentConstraints.xVars, seqDataset);
+			
+//			var groupingViolation = grpConstr.violateGroupingDecVars.stream()
+//				          .map(ConditionalGroupConstraint.GrpLinkedDecisionVar::asVar)
+//				          .map(grbVar -> Try.getting(() -> grbVar.get(GRB.DoubleAttr.X)).or(Rethrow.asRuntime()))
+//				          .collect(Collectors.toList());
+			
+			// assert all groups in final output
+			var numCliquesTogether = new AtomicInteger(0);
+			var matchingByProject = matching2.sequential().groupedByProject();
+			maxsizeCliques.asCollection().forEach(clique ->{
+				var cliqueIsTogether = matchingByProject.values().stream().anyMatch(agents -> agents.containsAll(clique.members().asCollection()));
+				if (cliqueIsTogether) numCliquesTogether.incrementAndGet();
+			});
+			
+			var matchingByRank = matching2.sequential().asList().stream()
+				.collect(Collectors.groupingBy(o -> {
+					var agent = o.from();
+					var project = o.to();
+					
+					return agent.projectPreference().rankOf(project);
+				}));
+			
+			Assert.that(numCliquesTogether.get() == maxsizeCliques.count())
+				.orThrowMessage("There is a clique not together, grp constr: " + grpConstr.simpleName());
+			
+			// EXPORT RESULTS
+			Assert.that(dataset.numMaxSlots() == 1)
+				.orThrowMessage("TODO: get mapping slot to agent (projects in dataset have more than 1 slot)");
+//			var csv = new ProjectStudentMatchingCSV(FormedGroupToProjectMatching.fromByTrivialPartitioning(matching2.original()));
+//			csv.writeToFile("research_project/research_proj " + objFn.name() + " 23_03_21 - w optional");
+			
+			
+			var report = new TwoRoundExperimentReport(matching.sequential(), matching2.sequential(),
+				seqDataset.allAgents(), individualAgents, groupingAgents, indifferentAgents);
+			
+			report.asHtmlReport()
+				.writeHtmlSourceToFile(new File("reports/test/" + ce + "_" + grpConstr.simpleName() + "_" + objFn.name() + "" + ".html"));
+			
+		}
+		catch (GRBException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private static void applyManualMatchFixes(MatchFixes matchFixes, GRBModel model, AssignmentConstraints assignmentConstraints) throws GRBException
+	{
+		for (var matchFix : matchFixes.asList())
+		{
+			if (matchFix.group().members().count() > 1) {
+				new GroupConstraint(Groups.of(matchFix.group()))
+					.apply(model, assignmentConstraints);
+			}
+			
+			for (var agent : matchFix.group().members())
+			{
+				var matchFixConstraint = new FixMatchingConstraint(agent, matchFix.project());
+				matchFixConstraint.apply(model, assignmentConstraints);
+			}
+		}
+	}
+	
+	private static Profile.listBased profileOfIndividualAgentsInMatching(SequentualDatasetContext seqDatasetContext, Agents individualAgents, AgentToProjectMatching matching)
+	{
+		return matching.asList().stream()
+			       // Only agents that are 'individual'
+			       .filter(match -> individualAgents.findByAgentId(match.from().id).isPresent())
+			       // A profile is a sorted list of ranks
+			       .map(match -> {
+				       var rank = match.from().projectPreference().rankOf(match.to());
+				       Assert.that(rank.isPresent()).orThrowMessage("Rank not present, handle this case");
+				       return rank.asInt();
+			       })
+			       .sorted()
+			       .collect(collectingAndThen(toList(), Profile.listBased::new));
+	}
+
+	private static DatasetContext datasetCE10()
+	{
+		DatasetContext dataContext = CourseEdition.fromLocalBepSysDbSnapshot(10);
+		return dataContext;
+	}
+	
+	private static DatasetContext datasetResearchProj21()
+	{
+		var dataContext = CourseEdition.fromLocalBepSysDbSnapshot(39);
+		
+		return dataContext;
+	}
+
+}
+
