e4253bf8a4f09886f6ff82e1a9b86198bd2678ca
Philipe Louchtch <philipelouchtch@outlook.com>
2021-10-19 21:32:27 +0200

    fix hard group constraint for SDU datasets

--- src/main/java/nl/tudelft/aidm/optimalgroups/algorithm/holistic/chiarandini/constraints/grouping/HardGroupingConstraint.java
+++ src/main/java/nl/tudelft/aidm/optimalgroups/algorithm/holistic/chiarandini/constraints/grouping/HardGroupingConstraint.java
@@ -36,7 +36,7 @@ public record HardGroupingConstraint(Groups<?> groups) implements Constraint
 						assignmentConstraints.xVars.of(agent1, slot).ifPresent(Xa1ToSlot -> {
 							assignmentConstraints.xVars.of(agent2, slot).ifPresent(Xa2ToSlot -> {
 								Try.doing(() ->
-								          model.addConstr(Xa1ToSlot.asVar(), '=', Xa2ToSlot.asVar(), "cnstr_grp_" + agent1 + "_" + agent2 + "_to_" + slot)
+								          model.addConstr(Xa1ToSlot.asVar(), '=', Xa2ToSlot.asVar(), "cnstr_grp_" + agent1.sequenceNumber() + "_" + agent2.sequenceNumber() + "_to_" + slot)
 								).or(Rethrow.asRuntime());
 							});
 						});
